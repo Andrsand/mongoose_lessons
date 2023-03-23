97b0fd7926f774252874b384eee0853b62d01620
andrsand <angorko@list.ru>
2023-03-23 08:26:54 +0300

    Search for documents by specific properties

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/usersdb");
 
     // получаем все объекты из БД
-    const users = await User.find({}); // Если в качестве критерия фильтрации передаются пустые фигурные скобки ({}), то возвращаются все объекты
+    const users = await User.find({ name: "Tom" }); // поиск докумета по определенным свойствам. В данном случае по имени
     console.log(users);
 }
 main().catch(console.log).finally(async () => await mongoose.disconnect());
