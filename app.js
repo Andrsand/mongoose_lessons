62d30671a8de48f1f22816b565c6b54de95f6137
andrsand <angorko@list.ru>
2023-03-23 08:45:01 +0300

    Refreshing a document with id filtering

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ async function main() {
 
     await mongoose.connect("mongodb://127.0.0.1:27017/usersdb");
 
-    // удаляем все объекты из БД, у которых age=34
-    const result = await User.deleteMany({ age: 34 });
-    console.log(result);
+    // обновление с фильтрацией по id
+    const id = "641bddcf9b968d1644d1786f";
+    const user = await User.findByIdAndUpdate(id, { name: "Sam", age: 25 });
+    console.log("Обновленный объект", user);
 }
 main().catch(console.log).finally(async () => await mongoose.disconnect());
