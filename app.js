46d3ab453da00f44acc18a27db2f886ac2dc8e3e
andrsand <angorko@list.ru>
2023-03-23 08:32:31 +0300

    Deleting documents by specific properties

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ async function main() {
 
     await mongoose.connect("mongodb://127.0.0.1:27017/usersdb");
 
-    // получаем все объекты из БД
-    const users = await User.find({ name: "Tom" }); // поиск докумета по определенным свойствам. В данном случае по имени
-    console.log(users);
+    // удаляем все объекты из БД, у которых age=34
+    const result = await User.deleteMany({ age: 34 });
+    console.log(result);
 }
 main().catch(console.log).finally(async () => await mongoose.disconnect());
