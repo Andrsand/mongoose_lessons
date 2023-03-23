0b12d093d4c93a4682984988c425a6a9a5c3c308
andrsand <angorko@list.ru>
2023-03-23 08:12:38 +0300

    Getting all data from a collection

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// установка схемы со значениями по умолчанию
-const userScheme = new Schema({
-    name: {
-        type: String,
-        default: "NoName"
-    },
-    age: {
-        type: Number,
-        default: 22
-    }
-});
-// определяем модель User
-const User = mongoose.model("User", userScheme); // Mongoose затем будет автоматически искать в базе данных коллекцию, название которой соответствует названию модели во множественном числе, т.е. users
-//  Если такой коллекции в базе данных нет, то она будет создана автоматически
+const userScheme = new Schema({ name: String, age: Number });
+const User = mongoose.model("User", userScheme);
 
 async function main() {
-    // подключемся к базе данных
+
     await mongoose.connect("mongodb://127.0.0.1:27017/usersdb");
 
-    // добавляем объект user в базе данных
-    const user = await User.create({ name: "Sam", age: 28 })
-    console.log(user);
+    // получаем все объекты из БД
+    const users = await User.find({}); // Если в качестве критерия фильтрации передаются пустые фигурные скобки ({}), то возвращаются все объекты
+    console.log(users);
 }
-// запускаем подключение и взаимодействие с базой данных
 main().catch(console.log).finally(async () => await mongoose.disconnect());
