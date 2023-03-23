cbb2aa8edccd87cf1381d6ce6ef531da97ca1694
andrsand <angorko@list.ru>
2023-03-23 07:56:15 +0300

    Creating a User Model Object with Default Values

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// установка схемы
+// установка схемы со значениями по умолчанию
 const userScheme = new Schema({
-    name: String,
-    age: Number
+    name: {
+        type: String,
+        default: "NoName"
+    },
+    age: {
+        type: Number,
+        default: 22
+    }
 });
 // определяем модель User
 const User = mongoose.model("User", userScheme); // Mongoose затем будет автоматически искать в базе данных коллекцию, название которой соответствует названию модели во множественном числе, т.е. users
 //  Если такой коллекции в базе данных нет, то она будет создана автоматически
 
-// создаем объект модели User
-const user = new User({ name: "Bill", age: 41 });
+// создаем объекты модели User в т.ч. со значениями по умолчанию
+const user = new User(); // name - NoName, age - 22
+
+console.log(user);
 
 async function main() {
     // подключемся к базе данных
