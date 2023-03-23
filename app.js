f1e48e460a476094be09bd45949ce0fac85b833a
andrsand <angorko@list.ru>
2023-03-23 08:06:37 +0300

    Adding an object to the database

--- app.js
+++ app.js
@@ -16,21 +16,13 @@ const userScheme = new Schema({
 const User = mongoose.model("User", userScheme); // Mongoose затем будет автоматически искать в базе данных коллекцию, название которой соответствует названию модели во множественном числе, т.е. users
 //  Если такой коллекции в базе данных нет, то она будет создана автоматически
 
-// создаем объекты модели User в т.ч. со значениями по умолчанию
-const user = new User(); // name - NoName, age - 22
-
-console.log(user);
-
 async function main() {
     // подключемся к базе данных
     await mongoose.connect("mongodb://127.0.0.1:27017/usersdb");
 
-    // сохраняем модель user в базу данных
-    await user.save();
-    console.log("Сохранен объект", user);
-
-    // отключаемся от базы данных
-    await mongoose.disconnect();
+    // добавляем объект user в базе данных
+    const user = await User.create({ name: "Sam", age: 28 })
+    console.log(user);
 }
 // запускаем подключение и взаимодействие с базой данных
-main().catch(console.log);
+main().catch(console.log).finally(async () => await mongoose.disconnect());
