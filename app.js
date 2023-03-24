acc3365dd3294e856b562c36367e9ec64ea6305d
andrsand <angorko@list.ru>
2023-03-24 23:58:00 +0300

    express & mongodb

--- app.js
+++ app.js
@@ -1,16 +1,81 @@
 const mongoose = require("mongoose");
+const express = require("express");
+const { isDate } = require("moment");
 const Schema = mongoose.Schema;
+const app = express();
+const jsonParser = express.json();
 
-const userScheme = new Schema({ name: String, age: Number });
+const userScheme = new Schema({ name: String, age: String }, { versionKey: false });
 const User = mongoose.model("User", userScheme);
+
+app.use(express.static(__dirname + "/public"));
 
 async function main() {
 
-    await mongoose.connect("mongodb://127.0.0.1:27017/usersdb");
-
-    // обновление с фильтрацией по id
-    const id = "641bddcf9b968d1644d1786f";
-    const user = await User.findByIdAndUpdate(id, { name: "Sam", age: 25 });
-    console.log("Обновленный объект", user);
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/usersdb");
+        app.listen(3000);
+        console.log("Сервер ожидает подключения...");
+    }
+    catch (err) {
+        return console.log(err);
+    }
 }
-main().catch(console.log).finally(async () => await mongoose.disconnect());
+
+app.get("/api/users", async (req, res) => {
+    // получаем всех пользователей
+    const users = await User.find({});
+    res.send(users);
+});
+
+app.get("/api/users/:id", async (req, res) => {
+
+    const id = req.params.id;
+    // получаем одного пользователя по id
+    const user = await User.findById(id);
+    if (user) res.send(user);
+    else res.sendStatus(404);
+});
+
+app.post("/api/users", jsonParser, async (req, res) => {
+
+    if (!req.body) return res.sendStatus(400);
+
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const user = new User({ name: userName, age: userAge });
+    // сохраняем в бд
+    await user.save();
+    res.send(user);
+});
+
+app.delete("/api/users/:id", async (req, res) => {
+
+    const id = req.params.id;
+    // удаляем по id 
+    const user = await User.findByIdAndDelete(id);
+    if (user) res.send(user);
+    else res.sendStatus(404);
+});
+
+app.put("/api/users", jsonParser, async (req, res) => {
+
+    if (!req.body) return res.sendStatus(400);
+    const id = req.body.id;
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const newUser = { age: userAge, name: userName };
+    // обновляем данные пользователя по id
+    const user = await User.findOneAndUpdate({ _id: id }, newUser, { new: true });
+    if (user) res.send(user);
+    else res.sendStatus(404);
+});
+
+main();
+// прослушиваем прерывание работы программы (ctrl-c)
+process.on("SIGINT", async () => {
+
+    await mongoose.disconnect();
+    console.log("Приложение завершило работу");
+    process.exit();
+});
